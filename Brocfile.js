304d79f467ff804d2d4f472055a66868a246e9df
Vicente Reig Rincon de Arellano <vicente@newrelic.com>
2014-12-28 14:54:15 +0100

    Moved imports to index.js

--- Brocfile.js
+++ Brocfile.js
@@ -4,8 +4,4 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
-app.import('bower_components/metrics-graphics/dist/metricsgraphics.css');
-app.import('bower_components/d3/d3.min.js');
-app.import('bower_components/metrics-graphics/dist/metricsgraphics.min.js');
-
 module.exports = app.toTree();
